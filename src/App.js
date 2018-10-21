1b2388b95ec342133cecb8596f75a4d6adb95fb2
William Kwok <wkwok16@uw.edu>
2018-10-20 17:05:48 -0700

    Got router up and running

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import firebase from 'firebase/app';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
+
+
+const Loading = () => <div></div>;
+const Main = Loadable({
+    loader: () => import('./containers/Main'),
+    loading: Loading,
+})
+
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
     render() {
         return (
             <div className="App">
-
+                <Router>
+                    <Switch>
+                        <Route exact path="/about" render={() => <div>About page</div>} />
+                        <Route render={() => <Main />} />
+                    </Switch>
+                </Router>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app';
+
+// import firebase
+let config = {
+    apiKey: "AIzaSyAZaMPQd-aOdFZjOCJpMS8U_bwqnMAC-gU",
+    authDomain: "findc-4a7e5.firebaseapp.com",
+    databaseURL: "https://findc-4a7e5.firebaseio.com",
+    projectId: "findc-4a7e5",
+    storageBucket: "findc-4a7e5.appspot.com",
+    messagingSenderId: "814470540647"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
